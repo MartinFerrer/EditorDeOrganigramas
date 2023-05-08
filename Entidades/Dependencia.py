49993f23bd41189a0b97cb192903f0c183768fcd
Javier Goto <132099390+JavierGoto02@users.noreply.github.com>
2023-05-07 20:18:49 -0400

    Genere codigos para dependencia

--- Entidades/Dependencia.py
+++ Entidades/Dependencia.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 import param
+from random import randint
+from typing import List
 
 @dataclass
 class Dependencia(param.Parameterized):
@@ -13,10 +15,28 @@ class Dependencia(param.Parameterized):
 
     nombre: str = param.String(None, regex="^.{0,20}$")
     """Nombre de la dependencia (hasta 20 caracteres)"""
-   
+
+    def __post_init__(self):
+        self.codigo = Dependencia.genera_code(self)
     # Representacion accessible para el usuario (#TODO: posiblemente remover si vamos a tener todo
     # en interfaz grafica)
     def __str__(self):
         return f"Dependencia: {self.nombre}, Código: {self.codigo}, Responsable: {self.codigoResponsable}"
     
     # TODO: Generar codigo unico entre 0 y 999
+    def genera_code(self):
+        code = randint(0, 100)
+        if code < 10:
+            code = "00" + str(code)
+        elif code >= 10 and code < 100:
+            code = "0" + str(code)
+        else:
+            code = str(code)
+
+        # if code not in lista de codigos:
+        #     lista de codigos.append(code)
+        return code
+
+Persona1 = Dependencia()
+print(Persona1.codigo)
+        
