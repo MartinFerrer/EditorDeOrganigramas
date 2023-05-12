97779ab1d4b48a239493d9ea278aa17fdb9bd324
Javier Goto <132099390+JavierGoto02@users.noreply.github.com>
2023-05-11 23:06:14 -0400

    Cambios

--- Archivo.py
+++ Archivo.py
@@ -64,55 +64,43 @@ class Archivo():
     # TODO: Implementar
     def graficarOrganigrama(nodo):
         pass
+
     
     # TODO: Implementar
     def graficarOrganigramaCompleto(self):
         pass
             
-    # TODO: Implementar
-    def crearDependencia(self, nom):
+    
+    def crearDependencia(self, nom): 
         cod = self.generarCodigoDependencia()
         dep = Dependencia(codigo = cod, codres = None, nombre = nom)
         self.dependenciasPorCodigo[cod] = dep
-        pass
+        
 
-    
-
-    def desAsignarPersona(self, codigo):
-        for elem in self.personasPorCodigo.values():
-                if elem.dependencia == codigo:
-                    elem.dependencia = None
-                    elem.salario = 0
-
-
-    def eliminarSuc(self, Base: 'NodoArbol'):
+    #Elimina los sucesores de la dependencia "Base" y desasigna a sus trabajadores (Funcion de desasignar esta en la linea 152).
+    def eliminarDependencia(self, Base: 'NodoArbol'): 
         for nodo in Base.children:
             self.eliminarDependencia(nodo)
             self.desAsignarPersona(nodo.data.codigo)
             self.dependenciasPorCodigo.pop(nodo.data.codigo)
             Base.children.remove(nodo)
         return
-    
 
-
-    def buscar_nodo(self, Base: 'NodoArbol', Raiz: 'NodoArbol'):
+    #Si borrarBase == True, borra el nodo "Base" y sus sucesores, si es False, solamente elimina el nodo "Base" 
+    # de los hijos de su antecesor.
+    def recorrerNodo(self, Base: 'NodoArbol', Raiz: 'NodoArbol', borrarBase):
         for nodo in Raiz.children:
             if nodo.data.codigo == Base.data.codigo:
-                self.eliminarDependencia(Base)
-                self.desAsignarPersona(nodo.data.codigo)
-                self.dependenciasPorCodigo.pop(nodo.data.codigo)
+                if borrarBase:
+                    self.eliminarDependencia(Base)
+                    self.desAsignarPersona(nodo.data.codigo)
+                    self.dependenciasPorCodigo.pop(nodo.data.codigo)
                 Raiz.children.remove(nodo)
             else:
-                self.buscar_nodo(Base, nodo)
-
-
-            
-            
-
-
+                self.recorrerNodo(Base, nodo, borrarBase)  
+   
 
     
-    # TODO: Implementar
     def modificarDependencia(self, cod_indice, nombre_nuevo, codres_nuevo):
         dep_destino = self.dependenciasPorCodigo[cod_indice]            
         if codres_nuevo in self.personasPorCodigo.keys():
@@ -120,29 +108,31 @@ class Archivo():
             dep_destino.codigoResponsable = codres_nuevo
         if nombre_nuevo != None: 
             dep_destino.nombre = nombre_nuevo
-        pass
 
-    # TODO: Implementar
-    def editarUbicacionDependencia():
-        pass
+    #Agrega en los hijos de nodoDestino el objeto nodoMover y elimina de los hijos del anterior padre.
+    def editarUbicacionDependencia(self, Raiz: 'NodoArbol', nodoMover: 'NodoArbol', nodoDestino: 'NodoArbol'):
+        nodoDestino.children.append(nodoMover)
+        self.recorrerNodo(nodoMover, Raiz, False)
+
+
     
-    # TODO: Implementar
+    
     def ingresarPersona(self, nom, ape, ci, tel, dir, salario):
         cod = self.generarCodigoPersona()
         persona = Persona(codigo = cod, nombre = nom, apellido = ape, documento = ci, telefono = tel, direccion = dir, salario = salario)
         self.personasPorCodigo[cod] = persona
-        pass
-    
-    # TODO: Implementar
+        
+
+ 
     def eliminarPersona(self, cod_indice):
         for elem in self.dependenciasPorCodigo.values():
             if elem.codigoResponsable == cod_indice:
                 elem.codigoResponsable = None
                 break
         self.personasPorCodigo.pop(cod_indice)
-        pass
-    
-    # TODO: Implementar
+        
+
+  
     def modificarPersona(self, persona_nueva : Persona):
         persona_destino = self.personasPorCodigo[persona_nueva.codigo]
         persona_destino.dependencia = persona_nueva.dependencia
@@ -151,11 +141,19 @@ class Archivo():
         persona_destino.telefono = persona_nueva.telefono
         persona_destino.direccion = persona_nueva.direccion
         persona_destino.salario = persona_nueva.salario
-        pass
+        
     
-    # TODO: Implementar
+  
     def asignarPersonaADependencia(self, cod_persona, dependenciaAsignada):
         persona = self.personasPorCodigo[cod_persona]
         persona.dependencia = dependenciaAsignada
-        pass
+        
+
+
+    def desAsignarPersona(self, codigo):
+        for elem in self.personasPorCodigo.values():
+                if elem.dependencia == codigo:
+                    elem.dependencia = None
+                    elem.salario = 0
+        
 
