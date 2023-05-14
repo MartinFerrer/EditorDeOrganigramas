3cdb081eec67709496db6a28380a218ba563e41a
Hirosamu12 <132099264+Hirosamu12@users.noreply.github.com>
2023-05-13 23:23:57 -0400

    informes(no totalmente funcional)
    el pathing de la fuente esta en mi pc.
    no incluye uso de datos del arbol.

--- Informes/Informes.py
+++ Informes/Informes.py
@@ -1,3 +1,17 @@
+from fpdf import FPDF
+import warnings
+
+warnings.simplefilter('default', DeprecationWarning)
+
+pdf = FPDF()
+pdf.add_font('Calibrinormal', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibri.ttf')
+pdf.add_font('Calibrilight', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibril.ttf')
+pdf.add_font('Calibricursiva', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibrii.ttf')
+pdf.add_font('Calibricursivalight', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibrili.ttf')
+pdf.add_font('Calibricursivanegrita', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibriz.ttf')
+pdf.add_font('Calibrinegrita', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibrib.ttf')
+
+
 # TODO: Implementar
 class Informes:
     def __init__(self) -> None:
@@ -5,8 +19,25 @@ class Informes:
     
     # TODO: Implementar
     def personalPorDependencia():
-        pass
-    
+        nomdep = (input("Inserte nombre de la dependencia: "))
+
+
+        #crear dpf
+        pdf.add_page()
+        pdf.set_font('Calibrinegrita', size=30)
+        pdf.cell(0, 20, f'Dependencia: {nomdep}')
+        nombre = ['Martin', 'Hiroto', 'Javier', 'Ivan', 'Fabrizio']
+        apellido = ['Ferrer', 'Yamashita', 'Goto', 'Figueredo', 'Kawabata']
+        
+        pdf.write(txt="\n\n")
+        pdf.set_font('Calibricursiva', size=28)
+        pdf.write(txt="Nombre y Apellido:\n")
+        pdf.set_font('Calibrinormal', size = 20)
+        for i in range(5):
+            pdf.cell(0,20, f'{nombre[i]} {apellido[i]}')
+            pdf.write(txt="\n\n")
+        pdf.output("Personal por Dependencia.pdf")
+        
     # TODO: Implementar
     def personalPorDependenciaExtendido():
         pass
@@ -22,3 +53,7 @@ class Informes:
     # TODO: Implementar
     def imprimirOrganigrama():
         pass
+
+
+
+    personalPorDependencia()
