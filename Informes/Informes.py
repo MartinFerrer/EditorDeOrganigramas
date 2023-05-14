97c1d057c12cdf5c2bcc997bf9b96262099c5032
Hirosamu12 <132099264+Hirosamu12@users.noreply.github.com>
2023-05-14 00:10:56 -0400

    informes(no funcional)
    mismo que anterior

--- Informes/Informes.py
+++ Informes/Informes.py
@@ -4,13 +4,9 @@ import warnings
 warnings.simplefilter('default', DeprecationWarning)
 
 pdf = FPDF()
-pdf.add_font('Calibrinormal', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibri.ttf')
-pdf.add_font('Calibrilight', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibril.ttf')
-pdf.add_font('Calibricursiva', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibrii.ttf')
-pdf.add_font('Calibricursivalight', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibrili.ttf')
-pdf.add_font('Calibricursivanegrita', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibriz.ttf')
-pdf.add_font('Calibrinegrita', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibrib.ttf')
-
+pdf.add_font('Calibri', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibri.ttf')
+pdf.add_font('CalibriBold', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibrib.ttf')
+pdf.add_font('CalibriItalic', '', r'C:\Users\hiros\OneDrive\Documentos\GitHub\EditorDeOrganigramas\Informes\fuentes\calibrii.ttf')
 
 # TODO: Implementar
 class Informes:
@@ -24,18 +20,22 @@ class Informes:
 
         #crear dpf
         pdf.add_page()
-        pdf.set_font('Calibrinegrita', size=30)
+        pdf.set_font('CalibriBold', '', size=30)
         pdf.cell(0, 20, f'Dependencia: {nomdep}')
         nombre = ['Martin', 'Hiroto', 'Javier', 'Ivan', 'Fabrizio']
         apellido = ['Ferrer', 'Yamashita', 'Goto', 'Figueredo', 'Kawabata']
         
         pdf.write(txt="\n\n")
-        pdf.set_font('Calibricursiva', size=28)
-        pdf.write(txt="Nombre y Apellido:\n")
-        pdf.set_font('Calibrinormal', size = 20)
+        pdf.set_font('Calibri', 'U' ,size=28)
+        pdf.write(txt="Nombre y Apellido")
+        pdf.set_font('Calibri', '', size=28)
+        pdf.write(txt=':\n')
+        pdf.set_font('CalibriItalic', '', size = 20)
         for i in range(5):
             pdf.cell(0,20, f'{nombre[i]} {apellido[i]}')
-            pdf.write(txt="\n\n")
+            pdf.set_font('CalibriItalic', '', size = 15)
+            pdf.write(txt=" \n\n")
+            pdf.set_font('CalibriItalic', '', size = 20)
         pdf.output("Personal por Dependencia.pdf")
         
     # TODO: Implementar
